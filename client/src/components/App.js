fb932edffb12f4c2f6e7f74f2a019bcd8138937c
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2019-11-30 12:25:00 -0500

    add a dropdown to select the year

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,8 +8,9 @@ class App extends React.Component {
 		super(props);
 
 		this.state = {
-			year: populateYear(2020)
-		}
+			year: populateYear(2020),
+			starredDays: []
+		};
 
 		this.monthRefs = this.state.year.reduce((refsObject, value, index) => {
 			refsObject[`month${index + 1}`] = React.createRef();
@@ -38,9 +39,22 @@ class App extends React.Component {
 		this.observer = new IntersectionObserver(intersectionCallback, {
 			threshold: new Array(101).fill(0).map((value, index, array) => index / (array.length - 1))
 		});
+
+		this.handleYearChange = this.handleYearChange.bind(this);
+	}
+
+	handleYearChange(event) {
+		const year = parseInt(event.target.value);
+		this.setState({ year: populateYear(year) });
 	}
 
 	componentDidMount() {
+		fetch('/api/stars')
+			.then(response => response.json())
+			.then(result => {
+				this.setState({ starredDays: result });
+			});
+
 		Object.values(this.monthRefs).forEach(value => {
 			this.observer.observe(value.current);
 		});
@@ -50,6 +64,15 @@ class App extends React.Component {
 		return (
 			<div className="container">
 				<h1>Sticker Calendar</h1>
+				<br/>
+				<label htmlFor="year-select">Year: </label>
+				<select id="year-select" onChange={this.handleYearChange}>
+					<option value="2020">2020</option>
+					<option value="2021">2021</option>
+					<option value="2022">2022</option>
+					<option value="2023">2023</option>
+				</select>
+				<br/>
 				<p>What is your goal?</p>
 				<select>
 					<option value="exercise">Exercise</option>
