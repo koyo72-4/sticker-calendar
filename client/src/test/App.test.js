b440fb23b44b4c5c890f3b4db35304bff260937c
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2020-01-31 21:14:34 -0500

    enable the use of Enzyme snapshot tests

--- client/src/test/App.test.js
+++ client/src/test/App.test.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { App } from '../components/App';
 
-it('renders without crashing', () => {
-	global.IntersectionObserver = class IntersectionObserver {
-		constructor() {}
+Enzyme.configure({ adapter: new Adapter() });
 
-		observe() {
-			return null;
-		}
+global.IntersectionObserver = class IntersectionObserver {
+	constructor() {}
+	observe() {
+		return null;
+	}
+	unobserve() {
+		return null;
+	}
+};
 
-		unobserve() {
-			return null;
-		}
-	};
+describe('App', () => {
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<App />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
 
-	const div = document.createElement('div');
-	ReactDOM.render(<App />, div);
-	ReactDOM.unmountComponentAtNode(div);
+	test('renders a calendar', () => {
+		const component = shallow(<App />);
+		expect(component).toMatchSnapshot();
+	});
 });
