a9c5cbdf3996af5ced2d113986d74b1602bbb045
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2020-02-22 18:12:46 -0500

    fix bug in which you couldn't search for a year in the early 1760's

--- client/src/components/YearSwitcher.js
+++ client/src/components/YearSwitcher.js
@@ -19,7 +19,7 @@ export const YearSwitcher = React.forwardRef((
                 name="year"
                 className="right-margin-20 left-padding-3"
                 required
-                pattern="^([1-9][7-9][5-9][3-9]|[1-9][8-9][0-9]{2}|[2-9][0-9]{3}|[1-9]\d{4})$"
+                pattern="^([1-9][7-9][5-9][3-9]|[1-9][7-9][6-9]\d|[1-9][8-9]\d{2}|[2-9]\d{3}|[1-9]\d{4})$"
                 title="Please enter a valid year between 1753 and 99999, inclusive."
                 value={yearInputValue}
                 onChange={handleYearInputChange}
